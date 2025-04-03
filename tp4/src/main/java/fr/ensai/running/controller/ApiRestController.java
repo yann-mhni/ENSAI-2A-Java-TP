844bf15a56bacdf534a34c060704f1bf84b5f2f6
ludo2ne <ludovic.deneuville@ensai.fr>
2025-04-03 14:43:00 +0000

    start tp4

---
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -0,0 +1,29 @@
+package fr.ensai.running.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.ensai.running.model.Athlete;
+import fr.ensai.running.service.AthleteService;
+
+@RestController
+@RequestMapping("/api")
+public class ApiRestController {
+
+    @Autowired
+    private AthleteService athleteService;
+
+    /**
+     * Get all athletes
+     */
+    @GetMapping("/athlete")
+    public List<Athlete> allAthletes() {
+
+        return athleteService.findAll();
+    }
+
+}
--- tp4/src/main/resources/data.sql
+++ tp4/src/main/resources/data.sql
@@ -13,16 +13,16 @@ CREATE TABLE athlete (
 );
 
 INSERT INTO athlete (id_athlete, first_name, last_name, age, gender) VALUES
-(99990, 'Bowie', 'Kent', 25, 'Female'),
-(99991, 'Amour', 'Hire De Rire', 30, 'Male'),
-(99992, 'Aubin', 'Sahalor', 22, 'Male'),
-(99993, 'Aude', 'Hébussavabien', 35, 'Female'),
-(99994, 'Bart', 'Haba', 28, 'Male'),
-(99995, 'Bernadette', 'Dejeu', 21, 'Female'),
-(99996, 'André', 'Céouver', 40, 'Male'),
-(99997, 'Aphonse', 'Kelpeuv', 27, 'Female'),
-(99998, 'Bill', 'Hattérahl', 32, 'Male'),
-(99999, 'Bianca', 'Lamine', 24, 'Female');
+(99990, 'Alain', 'Provist', 27, 'Male'),
+(99991, 'Amour', 'Hire De Rire', 32, 'Male'),
+(99992, 'Aude', 'Hébussavabien', 23, 'Female'),
+(99993, 'Bill', 'Hattérahl', 31, 'Male'),
+(99994, 'Armelle', 'Couvert', 25, 'Female'),
+(99995, 'Beth', 'Rave', 28, 'Female'),
+(99996, 'Daisy', 'Dyossi', 35, 'Female'),
+(99997, 'Douglas', 'Ochocola', 22, 'Male'),
+(99998, 'Helmut', 'Tardeellepique', 41, 'Male'),
+(99999, 'Ines', 'Pairet', 29, 'Female');
 
 
 ----------------------------------------------
@@ -46,47 +46,3 @@ INSERT INTO competition (id_competition, designation, city, event_date, distance
 (88882, 'Rennes Urban Trail', 'Rennes', '2024-04-27', 24, 750),
 (88883, 'Tour de l''île d''Arz', 'Île d''Arz', '2024-05-17', 10, 300),
 (88884, 'Trail de l''Enfer', 'Plouguiel', '2024-07-06', 18, 200);
-
-
-----------------------------------------------
--- Registration
-----------------------------------------------
-
-CREATE TABLE registration (
-    id_registration       SERIAL PRIMARY KEY,
-    id_athlete            BIGINT REFERENCES athlete(id_athlete),
-    id_competition        BIGINT REFERENCES competition(id_competition),
-    registration_date     DATE
-);
-
-INSERT INTO registration (id_registration, id_athlete, id_competition, registration_date) VALUES
-(77770, 99990, 88880, '2024-03-01'),
-(77771, 99991, 88881, '2024-03-02'),
-(77772, 99992, 88882, '2024-03-03'),
-(77773, 99993, 88883, '2024-03-04'),
-(77774, 99994, 88884, '2024-03-05'),
-(77775, 99995, 88880, '2024-03-06'),
-(77776, 99996, 88881, '2024-03-07'),
-(77777, 99997, 88882, '2024-03-08'),
-(77778, 99998, 88883, '2024-03-09'),
-(77779, 99999, 88884, '2024-03-10'),
-(77780, 99990, 88881, '2024-03-11'),
-(77781, 99991, 88882, '2024-03-12'),
-(77782, 99992, 88883, '2024-03-13'),
-(77783, 99993, 88884, '2024-03-14'),
-(77784, 99994, 88880, '2024-03-15'),
-(77785, 99995, 88882, '2024-03-16'),
-(77786, 99996, 88883, '2024-03-17'),
-(77787, 99997, 88884, '2024-03-18'),
-(77788, 99998, 88880, '2024-03-19'),
-(77789, 99999, 88881, '2024-03-20'),
-(77790, 99990, 88882, '2024-03-21'),
-(77791, 99991, 88883, '2024-03-22'),
-(77792, 99992, 88884, '2024-03-23'),
-(77793, 99993, 88880, '2024-03-24'),
-(77794, 99994, 88881, '2024-03-25'),
-(77795, 99995, 88883, '2024-03-26'),
-(77796, 99996, 88884, '2024-03-27'),
-(77797, 99997, 88880, '2024-03-28'),
-(77798, 99998, 88881, '2024-03-29'),
-(77799, 99999, 88882, '2024-03-30');
